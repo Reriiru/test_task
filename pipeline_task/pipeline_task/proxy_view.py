2a0a58b5ade374bf50bddc3de5113f695a521ddf
Honketh <ivelmey@gmail.com>
2017-10-09 02:22:51 +0300

    Found an issue, fixing it.

--- pipeline_task/pipeline_task/proxy_view.py
+++ pipeline_task/pipeline_task/proxy_view.py
@@ -1,12 +1,25 @@
 import urllib.request
 from django.http import HttpResponse
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 import re
 
 
 def tm_adder(matched_string):
     '''Adds the character'''
     return matched_string.group(1) + '™'
+
+
+def remove_comments(text):
+    '''Removes comments from Html'''
+    return isinstance(text, Comment)
+
+
+def func_checker(element):
+    if element.find_parent('script') and \
+       element.find_parent('style'):
+        return True
+    else:
+        return False
 
 
 def proxy_view(request):
@@ -19,14 +32,22 @@ def proxy_view(request):
     if headers['Content-Type'] == 'text/html; charset=UTF-8':
         soup = BeautifulSoup(content, 'html.parser')
 
-        for element in soup.find_all(text=True):
-            text = element.string.strip()
+        comments = soup.findAll(text=remove_comments)
+
+        for comment in comments:
+            if not func_checker(comment):
+                comment.extract()
+
+        elements = soup.body.find_all(text=True)
+        print(soup.body.find_all())
+        for element in elements:
+            text = None
+            if not func_checker(element):
+                text = element.string.strip()
             if text:
-                if not element.find_parent('script') and \
-                   not element.find_parent('head'):
-                    element.replace_with(re.sub(r'\b(\w{6})\b',
-                                                tm_adder,
-                                                text))
+                element.replace_with(re.sub(r'\b(\w{6})\b',
+                                            tm_adder,
+                                            text))
 
         return HttpResponse(soup.prettify())
 
